4ec03bbad4723934fbf4b904e585e4de4989a45e
Alban Kraus <alban.kraus@ensg.eu>
2015-10-14 18:03:13 +0200

    Cleared the background of openGL window

--- src/gldisplay.cpp
+++ src/gldisplay.cpp
@@ -2,10 +2,91 @@
 
 glDisplay::glDisplay(MainWindow * mainW) :
     QWindow(), // on appelle toujours le constructeur de la classe parente en premier
-    m_mainW(mainW)
+    QOpenGLFunctions(),
+    m_mainW(mainW),
+    m_context(new(std::nothrow) QOpenGLContext),
+    m_windowSize(400, 300),
+    m_initOK(false)
 {
+    setWidth(m_windowSize.width());
+    setHeight(m_windowSize.height());
+
+    // This window will be rendered with openGL
+    setSurfaceType(QWindow::OpenGLSurface);
+
+    // Creating the drawing context
+    if(m_context)
+    {
+        if(!m_context->create())
+            throw initException("Unable to create openGL context.");
+    }
+    else throw new std::bad_alloc;
 }
 
 glDisplay::~glDisplay()
 {
+    delete m_context;
 }
+
+void glDisplay::reshapeWindow(int width, int height)
+{
+    m_windowSize.setWidth(width);
+    m_windowSize.setHeight(height);
+}
+
+void glDisplay::initialize()
+{
+    // If the window is not exposed, it will not be drawn,
+    // so openGL will not know anything about the graphic processor ;
+    // it cannot be initialized, thus.
+    if(isExposed())
+    {
+        // Initialize openGL
+        initializeOpenGLFunctions();
+        // Background color
+        glClearColor(255, 255, 255, 255);
+
+        m_initOK = true;
+    }
+}
+
+void glDisplay::render()
+{
+    // Ensure the right drawing context will be used
+    m_context->makeCurrent(this);
+
+    if(!m_initOK)
+        initialize();
+
+    if(m_initOK) // else, cannot draw anything
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT); // clear screen
+
+        m_context->swapBuffers(this);
+    }
+}
+
+bool glDisplay::event(QEvent * event)
+{
+    bool processed = false; // Has the event been successfully processed?
+    switch (event->type())
+    {
+    case QEvent::UpdateRequest:
+        render();
+        processed = true;
+        break;
+    default:
+        // pass the event to the parent class to have the default behavior
+        processed = QWindow::event(event);
+    }
+    return processed;
+}
+
+void glDisplay::exposeEvent(QExposeEvent *event)
+{
+    Q_UNUSED(event); // prevent the event to be processed
+
+    if (isExposed())
+        render();
+}
+
--- src/gldisplay.h
+++ src/gldisplay.h
@@ -1,25 +1,57 @@
 #ifndef GL_H
 #define GL_H
 
+#include <stdexcept>
+
 #include <QWindow>
+#include <QOpenGLFunctions>
+#include <QOpenGLContext>
 
-
+// Declare a class MainWindow to resolve circular inclusion
 class MainWindow;
+
 
 /* Window where OpenGL graphics will be done.
  *
  * Upon upgrading to Qt 5.4, adapt it to a QOpenGLWidget instead of a QWindow
  */
-class glDisplay : public QWindow
+class glDisplay : public QWindow, protected QOpenGLFunctions
 {
     Q_OBJECT
 
+public:
+    // Exception raised during window initialization
+    class initException : public std::runtime_error
+    {
+    public:
+        initException(const char* message) : std::runtime_error(message) {}
+    };
+
 private:
     MainWindow * const m_mainW; // A pointer in order to communicate with main window
+    QOpenGLContext * const m_context; // The context responsible for the drawing
+
+protected:
+    QSize m_windowSize;
+    bool m_initOK; // OpenGL has (or not) been initialized
 
 public:
     explicit glDisplay(MainWindow *mainW);
     ~glDisplay();
+
+    // Contains initialization code which cannot be executed before the window has been shown
+    virtual void initialize();
+
+
+public slots:
+    void render(); // drawing function
+    void reshapeWindow(int width, int height);
+
+protected:
+    // Function answering all events
+    bool event(QEvent * event);
+    // Function answering the ExposureEvent: called when the window exposure changes
+    void exposeEvent(QExposeEvent * event);
 };
 
 #endif // GL_H
